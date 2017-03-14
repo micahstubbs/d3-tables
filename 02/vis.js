42be6c6a271c5adf5e313072be831f75ecfcdd8f
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-14 16:32:56 -0700

    convert to es2015 with lebab

--- 02/vis.js
+++ 02/vis.js
@@ -1,4 +1,4 @@
-      var table = d3.select(".table-container").append("table");
+      const table = d3.select(".table-container").append("table");
       table.append("thead");
       table.append("tbody");
 
@@ -10,73 +10,73 @@
       function ready(error, qcew, stateface) {
         if (error) throw error;
 
-        var columns = [
+        const columns = [
           {
             head: "State",
             cl: "state",
-            html: function(row) {
-              var sf_letter = stateface[row.state_abbrev];
-              var icon = '<span class="stateface">' + sf_letter + '</span>';
-              var text = '<span class="title">' + row.state + '</span>';
+            html(row) {
+              const sf_letter = stateface[row.state_abbrev];
+              const icon = `<span class="stateface">${sf_letter}</span>`;
+              const text = `<span class="title">${row.state}</span>`;
               return icon + text;
             }
           },
           {
             head: "Employment (millions)",
             cl: "emp",
-            html: function(row) {
-              var scale = scale = d3.scale.threshold()
+            html(row) {
+              let scale = d3.scale.threshold()
                 .domain([1, 2, 4, 6])
                 .range([1, 2, 3, 4, 5]);
 
-              var icon = '<span class="fa fa-male"></span>';
-              var value = d3.format(",.1f")(row.emp/1000000);
-              var nIcons = scale(value);
-              var text = '<span class="text">' + value + '</span>';
+              const icon = '<span class="fa fa-male"></span>';
+              const value = d3.format(",.1f")(row.emp/1000000);
+              const nIcons = scale(value);
+              const text = `<span class="text">${value}</span>`;
               return text + d3.range(nIcons)
-                .map(function() { return icon; }).join("");
+                .map(() => icon).join("");
             }
           },
           {
             head: "Change in Employment",
             cl: "emp_pc",
-            html: function(row) {
-              var scale = d3.scale.threshold()
+            html(row) {
+              const scale = d3.scale.threshold()
                 .domain([0, .045])
                 .range(["down", "right", "up"]);
-              var icon = '<span class="fa fa-arrow-' + scale(row.emp_pc) +'"></span>';
-              var value = d3.format(",.0%")(row.emp_pc);
-              var text = '<span class="text">' + value + '</span>';
+              const icon = `<span class="fa fa-arrow-${scale(row.emp_pc)}"></span>`;
+              const value = d3.format(",.0%")(row.emp_pc);
+              const text = `<span class="text">${value}</span>`;
               return text + icon;
             }
           },
           {
             head: "Wage (weekly)",
             cl: "wage",
-            html: function(row) {
-              var scale = d3.scale.threshold()
+            html(row) {
+              const scale = d3.scale.threshold()
                 .domain([850, 1000])
                 .range([1, 2, 3]);
 
-              var icon = '<span class="fa fa-money fa-rotate-90"></span>';
-              var nIcons = scale(row.wage);
-              var value = d3.format("$,")(row.wage);
-              var text = '<span class="text">' + value + '</span>';
+              const icon = '<span class="fa fa-money fa-rotate-90"></span>';
+              const nIcons = scale(row.wage);
+              const value = d3.format("$,")(row.wage);
+              const text = `<span class="text">${value}</span>`;
               return text + d3.range(nIcons)
-                .map(function() { return icon; }).join("");
+                .map(() => icon).join("");
             }
           },
           {
             head: "Change in Wage",
             cl: "wage_pc",
-            html: function(row) {
-              var scale = d3.scale.threshold()
+            html(row) {
+              const scale = d3.scale.threshold()
                 .domain([0, .07])
                 .range(["down", "right", "up"]);
 
-              var icon = '<span class="fa fa-arrow-' + scale(row.wage_pc) +'"></span>';
-              var value = d3.format(",.0%")(row.wage_pc);
-              var text = '<span class="text">' + value + '</span>';
+              const icon = `<span class="fa fa-arrow-${scale(row.wage_pc)}"></span>`;
+              const value = d3.format(",.0%")(row.wage_pc);
+              const text = `<span class="text">${value}</span>`;
               return text + icon;
             }
           }
@@ -90,43 +90,39 @@
             .selectAll("th")
               .data(columns)
             .enter().append("th")
-              .attr("class", function(d) { return d.cl; })
-              .text(function(d) { return d.head; })
-              .on("click", function(d) {
-                var ascending = d.ascending ? false : true;
+              .attr("class", d => d.cl)
+              .text(d => d.head)
+              .on("click", d => {
+                const ascending = d.ascending ? false : true;
                 d.ascending = ascending;
 
-                qcew.sort(function(a, b) {
-                  return ascending ?
-                    d3.ascending(a[d.cl], b[d.cl]) :
-                    d3.descending(a[d.cl], b[d.cl]);
-                });
+                qcew.sort((a, b) => ascending ?
+                  d3.ascending(a[d.cl], b[d.cl]) :
+                  d3.descending(a[d.cl], b[d.cl]));
                 table.call(renderTable);
               });
 
-          var tr = table.select("tbody").selectAll("tr").data(qcew);
+          const tr = table.select("tbody").selectAll("tr").data(qcew);
 
           tr.enter().append("tr")
             .on("mouseenter", mouseenter)
             .on("mouseleave", mouseleave);
 
-          var td = tr.selectAll("td")
-              .data(function(row, i) {
-                return columns.map(function(c) {
-                  var cell = {};
-                   d3.keys(c).forEach(function(k) {
-                       cell[k] = typeof c[k] == 'function' ? c[k](row,i) : c[k];
-                   });
-                   return cell;
-                })
-              })
+          const td = tr.selectAll("td")
+              .data((row, i) => columns.map(c => {
+            const cell = {};
+             d3.keys(c).forEach(k => {
+                 cell[k] = typeof c[k] == 'function' ? c[k](row,i) : c[k];
+             });
+             return cell;
+          }));
 
           td.enter().append("td")
-            .attr("class", function(d) { return d.cl; })
+            .attr("class", d => d.cl)
             .style("background-color", "#fff")
             .style("border-bottom", ".5px solid white");
 
-          td.html(function(d) { return d.html; });
+          td.html(d => d.html);
         }
       }
 
